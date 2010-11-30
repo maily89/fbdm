fa2c85a5a3877a55140065bbce3246c0bec33109
longnhi.kiemthanh <longnhi.kiemthanh@d234c362-8417-b006-dc4c-d754b9ca569c>
2010-11-30 04:18:19 +0000

    Mind Map cho meeting ngày mai nè

---
+++ trunk/Process_Management/Meetings_Management/MEETING 01122010.mm
@@ -0,0 +1,52 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1291089558546" ID="Freemind_Link_213645661" MODIFIED="1291089571234" TEXT="MEETING 01/12/2010">
+<node CREATED="1291089572500" ID="_" MODIFIED="1291089581593" POSITION="right" TEXT="REQUIREMENTS">
+<node CREATED="1291089610671" ID="Freemind_Link_1164794881" MODIFIED="1291089624000" TEXT="Xem l&#x1ea1;i c&#xe1;c v&#x1ea5;n &#x111;&#x1ec1; v&#x1ec1; requirements">
+<node CREATED="1291089627593" ID="Freemind_Link_213916522" MODIFIED="1291089632968" TEXT="&#x110;&#xe3; &#x111;&#x1ea7;y &#x111;&#x1ee7; ch&#x1b0;a"/>
+<node CREATED="1291089633140" ID="Freemind_Link_626040984" MODIFIED="1291089637125" TEXT="C&#xf3; g&#xec; conflict kh&#xf4;ng"/>
+<node CREATED="1291089638281" ID="Freemind_Link_478230632" MODIFIED="1291089646500" TEXT="C&#xf3; y&#xea;u c&#x1ea7;u n&#xe0;o b&#x1ecb; th&#x1eeb;a kh&#xf4;ng"/>
+<node CREATED="1291089649375" ID="Freemind_Link_885741415" MODIFIED="1291089653859" TEXT="C&#xf3; y&#xea;u c&#x1ea7;u n&#xe0;o kh&#x1ea3; thi kh&#xf4;ng"/>
+</node>
+<node CREATED="1291089656171" ID="Freemind_Link_764588645" MODIFIED="1291089667375" TEXT="T&#x1ed5;ng k&#x1ebf;t l&#x1ea1;i c&#xe1;c requirements">
+<node CREATED="1291089667328" ID="Freemind_Link_131530445" MODIFIED="1291089689265" TEXT="Ph&#xe2;n chia l&#x1ea1;i module l&#x1edb;n cho chu&#x1ea9;n"/>
+<node CREATED="1291089689500" ID="Freemind_Link_402181489" MODIFIED="1291089713125" TEXT="&#x110;&#x1eb7;t t&#xea;n v&#xe0; c&#xe1;ch th&#x1ee9;c traceability c&#x169;ng nh&#x1b0; tracking th&#x1ead;t t&#x1ed1;t"/>
+</node>
+</node>
+<node CREATED="1291089581859" ID="Freemind_Link_906311259" MODIFIED="1291089593859" POSITION="right" TEXT="QUALITY ASSUARANCE">
+<node CREATED="1291089722859" ID="Freemind_Link_180585972" MODIFIED="1291089745265" TEXT="Giang v&#xe0; Th&#x1ea3;o s&#x1ebd; gi&#x1edb;i thi&#x1ec7;u chung v&#x1ec1; c&#xf4;ng vi&#x1ec7;c QA cho nh&#xf3;m d&#x1ef1; &#xe1;n">
+<node CREATED="1291089745250" ID="Freemind_Link_1226845623" MODIFIED="1291089751828" TEXT="C&#xe1;c templates"/>
+<node CREATED="1291089752046" ID="Freemind_Link_1638219359" MODIFIED="1291089771812" TEXT="C&#xe1;c &#x111;&#x1ecb;nh h&#x1b0;&#x1edb;ng trong vi&#x1ec7;c develop TC v&#xe0; c&#xe1;c c&#xf4;ng vi&#x1ec7;c li&#xea;n quan"/>
+<node CREATED="1291089772046" ID="Freemind_Link_489567765" MODIFIED="1291089786859" TEXT="C&#xe1;ch th&#x1ee9;c qu&#x1ea3;n l&#xfd; quy tr&#xec;nh, log v&#xe0; x&#x1eed; l&#xfd; l&#x1ed7;i"/>
+</node>
+<node CREATED="1291089790640" ID="Freemind_Link_1812331547" MODIFIED="1291089801843" TEXT="C&#xe1;c v&#x1ea5;n &#x111;&#x1ec1; n&#xea;u ra cho c&#xf4;ng vi&#x1ec7;c QA">
+<node CREATED="1291089801843" ID="Freemind_Link_1053049569" MODIFIED="1291089807421" TEXT="Integration test">
+<node CREATED="1291089823187" ID="Freemind_Link_394130124" MODIFIED="1291089831921" TEXT="B&#xe1;m v&#xe0;o requirements c&#xf3; &#x111;&#x1ea7;y &#x111;&#x1ee7; h&#x1ebf;t kh&#xf4;ng"/>
+<node CREATED="1291089833390" ID="Freemind_Link_1576284007" MODIFIED="1291089842281" TEXT="B&#xe1;m v&#xe0;o design nh&#x1b0; th&#x1ebf; n&#xe0;o"/>
+</node>
+<node CREATED="1291089807734" ID="Freemind_Link_477758107" MODIFIED="1291089812125" TEXT="Unit test">
+<node CREATED="1291089843531" ID="Freemind_Link_360745188" MODIFIED="1291089847890" TEXT="Ai s&#x1ebd; l&#xe0;m unit test"/>
+<node CREATED="1291089848265" ID="Freemind_Link_1337209111" MODIFIED="1291089871765" TEXT="L&#xe0;m unit test tr&#x1b0;&#x1edb;c hay sau khi code"/>
+<node CREATED="1291089885187" ID="Freemind_Link_1200958512" MODIFIED="1291089891125" TEXT="C&#xe1;ch th&#x1ee9;c l&#xe0;m th&#x1ebf; n&#xe0;o"/>
+</node>
+<node CREATED="1291089895171" ID="Freemind_Link_1568589386" MODIFIED="1291089909984" TEXT="C&#xe1;ch th&#x1ee9;c m&#xe0; n&#xf3; li&#xea;n quan &#x111;&#x1ebf;n requirements v&#xe0; design"/>
+</node>
+</node>
+<node CREATED="1291089594046" ID="Freemind_Link_1418067714" MODIFIED="1291089604640" POSITION="right" TEXT="ARCHITECTURAL DESIGN">
+<node CREATED="1291089912171" ID="Freemind_Link_393151105" MODIFIED="1291089938859" TEXT="Long propose c&#xe1;c m&#xf4; h&#xec;nh c&#xf3; th&#x1ec3; s&#x1eed; d&#x1ee5;ng, c&#xe1;c architectural styles"/>
+<node CREATED="1291089940562" ID="Freemind_Link_1890798035" MODIFIED="1291089954125" TEXT="C&#x1ea3; nh&#xf3;m s&#x1ebd; th&#x1ea3;o lu&#x1ead;n v&#xe0; l&#x1ef1;a ch&#x1ecd;n c&#xe1;c m&#xf4; h&#xec;nh th&#xed;ch h&#x1ee3;p">
+<node CREATED="1291089955187" ID="Freemind_Link_552490399" MODIFIED="1291089963609" TEXT="&#x110;&#x1ea3;m b&#x1ea3;o quality attributes"/>
+<node CREATED="1291089965000" ID="Freemind_Link_1457124975" MODIFIED="1291089974015" TEXT="&#x110;&#x1ea3;m b&#x1ea3;o y&#xea;u c&#x1ea7;u t&#xed;nh n&#x103;ng"/>
+</node>
+<node CREATED="1291089978718" ID="Freemind_Link_1401192589" MODIFIED="1291089997093" TEXT="Trong bu&#x1ed5;i h&#x1ecd;p ph&#x1ea3;i th&#x1ed1;ng nh&#x1ea5;t &#x111;&#x1b0;&#x1ee3;c m&#xf4; h&#xec;nh, sau &#x111;&#xf3; v&#x1ec1; nh&#xe0; ti&#x1ebf;p t&#x1ee5;c ph&#xe1;t tri&#x1ec3;n"/>
+<node CREATED="1291089999765" ID="Freemind_Link_454084218" MODIFIED="1291090004765" TEXT="Ph&#xe2;n ra m&#xf4; h&#xec;nh, chia vi&#x1ec7;c"/>
+<node CREATED="1291090005000" ID="Freemind_Link_1078665821" MODIFIED="1291090027625" TEXT="T&#xec;m hi&#x1ec3;u s&#x1ef1; li&#xea;n quan &#x111;&#x1ebf;n Requirements v&#xe0; QA &#x111;&#x1ec3; ti&#x1ec7;n vi&#x1ec7;c traceability v&#xe0; tracking"/>
+<node CREATED="1291090031937" ID="Freemind_Link_932064046" MODIFIED="1291090041015" TEXT="Quan t&#xe2;m &#x111;&#x1eb7;c bi&#x1ec7;t 2 issues l&#x1edb;n">
+<node CREATED="1291090041000" ID="Freemind_Link_290057338" MODIFIED="1291090059562" TEXT="Web interface?"/>
+<node CREATED="1291090048203" ID="Freemind_Link_850711340" MODIFIED="1291090055109" TEXT="C&#xf3; n&#xea;n l&#xe0;m Data warehouse?"/>
+</node>
+</node>
+<node CREATED="1291090073171" ID="Freemind_Link_459111467" MODIFIED="1291090108437" POSITION="right" TEXT="Trong bu&#x1ed5;i h&#x1ecd;p, ph&#x1ea3;i nh&#x1ea5;n m&#x1ea1;nh v&#x1ea5;n &#x111;&#x1ec1; traceability, t&#x1ee9;c l&#xe0; s&#x1ef1; li&#xea;n quan v&#xe0; tracking gi&#x1eef;a c&#xe1;c giai &#x111;o&#x1ea1;n &#x111;&#x1ec3; sau n&#xe0;y &#x111;i&#x1ec1;u ch&#x1ec9;nh v&#xe0; update cho h&#x1ee3;p l&#xfd;"/>
+<node CREATED="1291090109765" ID="Freemind_Link_1760518427" MODIFIED="1291090124109" POSITION="right" TEXT="Thi&#x1ebf;t k&#x1ebf; Data base (ERD)"/>
+</node>
+</map>
