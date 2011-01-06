a1b109262f498c599d5371b5b84446f979d60923
longnhi.kiemthanh <longnhi.kiemthanh@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-01-06 16:41:48 +0000

    CÓ RẤT NHIỀU THỨ TRONG VIỆC QUẢN LÝ PROCESS ĐÃ ĐƯỢC COMMIT, MỌI NGƯỜI UPDATE VỀ NHÉ. TRONG ĐÓ CÓ: PROJECT PLAN, TASK MANAGEMENT, ISSUES LOG.

---
+++ Process_Management/Meetings_Management/Minutes_2010.01.03.mm
@@ -0,0 +1,32 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1293983201828" ID="Freemind_Link_163356984" MODIFIED="1293983292781" TEXT="MEETING 03-Jan-2011">
+<node CREATED="1293983724109" ID="Freemind_Link_1991601858" MODIFIED="1293984628968" POSITION="right" TEXT="B&#xe1;o c&#xe1;o t&#xe1;c v&#x1ee5; (20 minutes)">
+<node CREATED="1293983750343" ID="Freemind_Link_92041967" MODIFIED="1293983767640" TEXT="C&#xf4;ng vi&#x1ec7;c IT b&#xea;n Giang &amp; Th&#x1ea3;o"/>
+<node CREATED="1293983767859" ID="Freemind_Link_510394344" MODIFIED="1293983776625" TEXT="C&#xf4;ng vi&#x1ec7;c create v&#xe0; load DB c&#x1ee7;a Ch&#xed;nh"/>
+<node CREATED="1293983780078" ID="Freemind_Link_429961535" MODIFIED="1293983790328" TEXT="Vi&#x1ec7;c thi&#x1ebf;t k&#x1ebf; DB v&#xe0; SRS c&#x1ee7;a Long &amp; Ly"/>
+</node>
+<node CREATED="1293983324656" ID="Freemind_Link_252111130" MODIFIED="1293984636734" POSITION="right" TEXT="Architecture Design (40 minutes)">
+<node CREATED="1293983330859" ID="Freemind_Link_249800208" MODIFIED="1293983333781" TEXT="Ki&#x1ec3;m ch&#x1ee9;ng l&#x1ea1;i"/>
+<node CREATED="1293983334343" ID="Freemind_Link_436436863" MODIFIED="1293983355968" TEXT="X&#xe1;c &#x111;&#x1ecb;nh xem &#x111;&#x1ec3; m&#xf4; h&#xec;nh MVC hay &#x111;&#x1a1;n thu&#x1ea7;n l&#xe0; Layered"/>
+<node CREATED="1293983357484" ID="Freemind_Link_1981442380" MODIFIED="1293983375875" TEXT="N&#x1ebf;u &#x111;&#x1ec3; MVC th&#xec; c&#x1a1; ch&#x1ebf; ra sao"/>
+<node CREATED="1293983376093" ID="Freemind_Link_670167503" MODIFIED="1293983396265" TEXT="N&#x1ebf;u &#x111;&#x1ec3; Layered th&#xec; 3 t&#x1ea7;ng nh&#x1b0; trong design &#x111;&#xe3; &#x111;&#x1ee7; ch&#x1b0;a"/>
+<node CREATED="1293983403562" ID="Freemind_Link_1935931846" MODIFIED="1293983409125" TEXT="C&#xf3; option cho m&#xf4; h&#xec;nh n&#xe0;o kh&#xe1;c kh&#xf4;ng"/>
+</node>
+<node CREATED="1293983250875" ID="_" MODIFIED="1293984644375" POSITION="right" TEXT="Vi&#x1ec7;c thi&#x1ebf;t k&#x1ebf; GUI (30 minutes)">
+<node CREATED="1293983416812" ID="Freemind_Link_1100039636" MODIFIED="1293983428281" TEXT="Flow gi&#x1eef;a c&#xe1;c m&#xe0;n h&#xec;nh"/>
+<node CREATED="1293983431296" ID="Freemind_Link_1389961827" MODIFIED="1293983451281" TEXT="Thi&#x1ebf;t k&#x1ebf; giao di&#x1ec7;n b&#x1eb1;ng ASP.NET lu&#xf4;n hay l&#xe0; b&#x1eb1;ng c&#xf4;ng c&#x1ee5; kh&#xe1;c"/>
+</node>
+<node CREATED="1293983283375" ID="Freemind_Link_420024082" MODIFIED="1293984651125" POSITION="right" TEXT="Vi&#x1ec7;c thi&#x1ebf;t k&#x1ebf; c&#xe1;c Module (60 minutes)">
+<node CREATED="1293983466718" ID="Freemind_Link_974936891" MODIFIED="1293983485343" TEXT="Th&#x1ed1;ng nh&#x1ea5;t c&#xe1;c module n&#xe0;o cho t&#x1eeb;ng kh&#x1ed1;i ki&#x1ebf;n tr&#xfa;c h&#x1ec7; th&#x1ed1;ng"/>
+<node CREATED="1293983488750" ID="Freemind_Link_1782460507" MODIFIED="1293983559296" TEXT="Interface gi&#x1eef;a c&#xe1;c module ph&#x1ea3;i ra sao"/>
+<node CREATED="1293983566234" ID="Freemind_Link_1622143420" MODIFIED="1293983584250" TEXT="C&#x1ee5; th&#x1ec3; trong m&#x1ed7;i kh&#x1ed1;i n&#xea;n c&#xf3; c&#xe1;c l&#x1edb;p ch&#xed;nh n&#xe0;o"/>
+</node>
+<node CREATED="1293983588640" ID="Freemind_Link_327410378" MODIFIED="1293984660562" POSITION="right" TEXT="Th&#x1ed1;ng nh&#x1ea5;t qu&#xe1; tr&#xec;nh l&#xe0;m vi&#x1ec7;c (60 minutes)">
+<node CREATED="1293983598531" ID="Freemind_Link_149148485" MODIFIED="1293983613390" TEXT="C&#x1a1; ch&#x1ebf; ph&#xe2;n vi&#x1ec7;c thi&#x1ebf;t k&#x1ebf;"/>
+<node CREATED="1293983613640" ID="Freemind_Link_1035482113" MODIFIED="1293983626578" TEXT="C&#x1a1; ch&#x1ebf; review DD"/>
+<node CREATED="1293983627218" ID="Freemind_Link_566074197" MODIFIED="1293983634656" TEXT="C&#x1a1; ch&#x1ebf; log l&#x1ed7;i v&#xe0; communicate"/>
+<node CREATED="1293983694859" ID="Freemind_Link_245366134" MODIFIED="1293983700953" TEXT="Plan cho ph&#x1ea7;n thi&#x1ebf;t k&#x1ebf;"/>
+</node>
+</node>
+</map>
