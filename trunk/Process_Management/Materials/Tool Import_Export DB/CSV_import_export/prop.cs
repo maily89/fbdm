94bcbd217c203600f336eeb42dddff234982171a
dragontl2189 <dragontl2189@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-02-01 00:52:55 +0000

    Tool Import_Export DB

---
+++ trunk/Process_Management/Materials/Tool Import_Export DB/CSV_import_export/prop.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSV_import_export
+{
+
+	// properties
+	class prop
+	{
+		//******************************************************
+		// conection string - REPLACE it, if you need.
+        public static string sqlConnImportString = "Data Source=localhost;Initial Catalog=FBD2;Integrated Security=True";
+        public static string sqlConnExportString = "Data Source=localhost;Initial Catalog=FBD;Integrated Security=True";
+        //******************************************************
+
+
+	}
+}
