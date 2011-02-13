ad579ef508916a0ffdf2543791371a0758898721
longnhi.kiemthanh <longnhi.kiemthanh@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-02-13 19:53:42 +0000

    Lớp model mà chứa các message error (hiển thị thông báo lỗi từ hệ thống) và business error (lỗi ở nghiệp vụ nào) cũng như chỉ ra rằng sau khi click nút [Back to working business] thì nó biết mà quay trở về trang index của nghiệp vụ đó Các cái này đều sẽ để dạng hằng số trong file ConstantsContainer nhé

---
+++ trunk/Sources/Source_Codes/FBDSource/FBD/Models/Error.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FBD.Models
+{
+    public class Error
+    {
+        public String ErrorBusiness;
+
+        public String ErrorMessage;
+
+        public String BackToPage;
+    }
+}
