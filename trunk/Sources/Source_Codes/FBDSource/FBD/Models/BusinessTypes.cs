4e80124960e5afda3cdccb11f9e7217f9d4842e6
mailydao@gmail.com <mailydao@gmail.com@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-02-14 10:19:59 +0000

    Business Type

---
+++ trunk/Sources/Source_Codes/FBDSource/FBD/Models/BusinessTypes.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FBD.Models
+{
+    [MetadataType(typeof(BusinessTypeMetaData))] 
+    public partial class BusinessTypes
+    {
+
+        public static List<BusinessTypes> SelectTypes()
+        {
+            FBDEntities entities = new FBDEntities();
+            return entities.BusinessTypes.ToList();
+        }
+        public static BusinessTypes SelectTypeByID(string id)
+        {
+            FBDEntities entities = new FBDEntities();
+            var type = entities.BusinessTypes.First(i => i.TypeID == id);
+            return type;
+        }
+
+        public static BusinessTypes SelectTypeByID(string id,FBDEntities entities)
+        {
+            
+            var type = entities.BusinessTypes.First(i => i.TypeID == id);
+            return type;
+        }
+
+        public static void DeleteType(string id)
+        {
+            FBDEntities entities = new FBDEntities();
+            var type = BusinessTypes.SelectTypeByID(id,entities);
+            entities.DeleteObject(type);
+            entities.SaveChanges();
+        }
+
+        public static void EditType(BusinessTypes type)
+        {
+            FBDEntities entities = new FBDEntities();
+            var temp = BusinessTypes.SelectTypeByID(type.TypeID,entities);
+            temp.TypeName = type.TypeName;
+            entities.SaveChanges();
+        }
+
+        public static void AddType(BusinessTypes type)
+        {
+            FBDEntities entities = new FBDEntities();
+            entities.AddToBusinessTypes(type);
+            entities.SaveChanges();
+        }
+        public class BusinessTypeMetaData
+        {
+            [DisplayName("Type ID")]
+            [Required(ErrorMessage="Type ID is required")]
+            [StringLength(4)]
+            public string TypeID { get; set; }
+
+            [DisplayName("Type Name")]
+            [Required(ErrorMessage="Type Name is required")]
+            [StringLength(255)]
+            public string TypeName { get; set; }
+        }
+    }
+}
---
+++ trunk/Sources/Source_Codes/FBDSource/FBD/Views/BSNType/Index.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<IEnumerable<FBD.Models.BusinessTypes>>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+	Index
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+    <h2>Index</h2>
+    <%= TempData["Message"]!=null?TempData["Message"]:"" %>
+    <table>
+        <tr>
+            <th></th>
+            <th>
+                Type ID
+            </th>
+            <th>
+                Type Name
+            </th>
+        </tr>
+
+    <% foreach (var item in Model) { %>
+    
+        <tr>
+            <td>
+                <%= Html.ActionLink("Edit", "Edit", new { id=item.TypeID }) %> |
+                <%= Html.ActionLink("Delete", "Delete", new { id = item.TypeID }, new { onclick = "javascript:return confirm('Are you sure you wish to delete " + item.TypeName + "?');" })%>
+            </td>
+            <td>
+                <%= Html.Encode(item.TypeID) %>
+            </td>
+            <td>
+                <%= Html.Encode(item.TypeName) %>
+            </td>
+        </tr>
+    
+    <% } %>
+
+    </table>
+
+    <p>
+        <%= Html.ActionLink("Create New", "Add") %>
+    </p>
+
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="ScriptContent" runat="server">
+</asp:Content>
+
