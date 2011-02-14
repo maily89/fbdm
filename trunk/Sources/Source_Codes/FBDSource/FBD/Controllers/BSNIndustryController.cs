3d1725116fb1a2fa77561b1e0cfbcd344fd2dfd0
mailydao@gmail.com <mailydao@gmail.com@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-02-14 07:52:00 +0000

    Addin BSNLine

--- trunk/Sources/Source_Codes/FBDSource/FBD/Controllers/BSNIndustryController.cs
+++ trunk/Sources/Source_Codes/FBDSource/FBD/Controllers/BSNIndustryController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using FBD.Models;
-
+using FBD.ViewModels;
 
 namespace FBD.Controllers
 {
---
+++ trunk/Sources/Source_Codes/FBDSource/FBD/ViewModels/BSNLineViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FBD.Models;
+
+namespace FBD.ViewModels
+{
+    public class BSNLineViewModel
+    {
+        public BusinessLines BusinessLines { get; set; }
+        public List<BusinessIndustries> BusinessIndustries { get; set; }
+        public string IndustryID { get; set; }
+    }
+}
