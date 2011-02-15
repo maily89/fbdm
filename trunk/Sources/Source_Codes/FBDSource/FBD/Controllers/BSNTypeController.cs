18791bf334a592dd1f51ead253c2340d40632d77
mailydao@gmail.com <mailydao@gmail.com@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-02-15 03:48:51 +0000

    commit bsntypecontroller

---
+++ trunk/Sources/Source_Codes/FBDSource/FBD/Controllers/BSNTypeController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FBD.Models;
+
+namespace FBD.Controllers
+{
+    //TODO: check Rights
+    //TODO: check type name and id unique
+    public class BSNTypeController : Controller
+    {
+        //
+        // GET: /BSNType/
+        
+        public ActionResult Index()
+        {
+            var types = BusinessTypes.SelectTypes();
+            
+            return View(types);
+        }
+
+
+
+        //
+        // GET: /BSNType/Add
+
+        public ActionResult Add()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /BSNType/Add
+
+        [HttpPost]
+        public ActionResult Add(BusinessTypes type)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    BusinessTypes.AddType(type);
+                }
+                else throw new Exception();
+                TempData["Message"] = "Type ID "+type.TypeID+ " have been added sucessfully";
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                TempData["Message"] = ex.Message;
+                return View(type);
+            }
+        }
+        
+        //
+        // GET: /BSNType/Edit/5
+ 
+        public ActionResult Edit(string id)
+        {
+            var model = BusinessTypes.SelectTypeByID(id);
+            return View(model);
+        }
+
+        //
+        // POST: /BSNType/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(string id, BusinessTypes type)
+        {
+            try
+            {
+                
+                if (ModelState.IsValid)
+                {
+                    BusinessTypes.EditType(type);
+                    
+                }
+                else throw new ArgumentException();
+                TempData["Message"] = "TypeID "+ type.TypeID + " have been updated sucessfully";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                //TODO: Temporary error handle.
+                
+                TempData["Message"] = ex.Message;
+                return View(type);
+            }
+        }
+
+        //
+        // GET: /BSNType/Delete/5
+ 
+        public ActionResult Delete(string id)
+        {
+            BusinessTypes.DeleteType(id);
+            TempData["Message"] = "Type ID "+ id+" have been deleted sucessfully";
+            return RedirectToAction("Index");
+        }
+
+        
+    }
+}
