f4677e83bdcf3e4bf58c4221ee2152af93cca100
longnhi.kiemthanh <longnhi.kiemthanh@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-03-02 05:02:39 +0000

    THÊM COMMENT CODE CHO XỬ LÝ FI PROPORTION PHẦN NÀY GẦN NHƯ HOÀN THÀNH, CHỈ CÒN THIẾU MỖI CHECK TỔNG TỶ TRỌNG = 100% VÀ KIỂM TRA GIÁ TRỊ DECIMAL Ở PHÍA CLIENT CỦA TRƯỜNG PROPORTION THÔI. NẾU AI CÓ IDEA GIÚP ĐỠ THÌ BUZZ TỚ NHÉ. ĐANG QUAY QUA CODE PHẦN SCORE CHO TIẾT KIỆM TIME

--- trunk/Sources/Source_Codes/FBDSource/FBD/Controllers/FIProportionController.cs
+++ trunk/Sources/Source_Codes/FBDSource/FBD/Controllers/FIProportionController.cs
@@ -15,20 +15,26 @@ namespace FBD.Controllers
 
         public ActionResult Index()
         {
+            // The view model to be exchanged
             FIProportionViewModel viewModel = new FIProportionViewModel();
             try
             {
+                // The list of business industries to be binded to drop down list
                 List<BusinessIndustries> lstIndustries = new List<BusinessIndustries>();
 
                 lstIndustries = BusinessIndustries.SelectIndustries();
 
+                // If there is no industries available in the system
+                // then return empty-data View
                 if (lstIndustries.Count() < 1)
                     return View(viewModel);
 
+                // Create View model with the drop down list displays the first industry of the list
                 viewModel = BusinessFinancialIndexProportion.CreateViewModelByIndustry(lstIndustries[0].IndustryID);
             }
             catch (Exception)
             {
+                // Display error message
                 TempData["Message"] = CommonUtilities.Constants.ERR_DISPLAY_FIPROPORTION;
                 return View(viewModel);
             }
@@ -39,10 +45,12 @@ namespace FBD.Controllers
         [HttpPost]
         public ActionResult Index(FormCollection formCollection)
         {
+            // If the action posted to Controller is done by selecting drop down list of industries
             try
             {
                 if (formCollection["Industry"] != null)
                 {
+                    // Create View model with input industry selected from drop down list
                     FIProportionViewModel viewModelForSelectingIndustries = BusinessFinancialIndexProportion
                                                             .CreateViewModelByIndustry(formCollection["Industry"].ToString());
 
@@ -51,34 +59,43 @@ namespace FBD.Controllers
             }
             catch (Exception)
             {
+                // Display error message when displaying information
                 TempData["Message"] = CommonUtilities.Constants.ERR_DISPLAY_FIPROPORTION;
                 return RedirectToAction("Index");
             }
 
+            // If the action posted to Controller is Saving information from View
             try
             {
                 if (formCollection["Save"] != null)
                 {
                     FIProportionViewModel viewModelForSavingProportion = new FIProportionViewModel();
 
+                    // With each financial index row in the list posted from View
                     for (int i = 0; i < int.Parse(formCollection["NumberOfProportionRows"].ToString()); i++)
                     {
+                        // Create new row
                         FIProportionRowViewModel rowForSavingProportion = new FIProportionRowViewModel();
-
+                                                
                         if (formCollection["ProportionRows[" + i + "].Checked"] != null)
                         {
+                            // If the row [i] is checked by the checkbox
                             if (formCollection["ProportionRows[" + i + "].Checked"].ToString().Equals("true,false")
                                 || formCollection["ProportionRows[" + i + "].Checked"].ToString().Equals("True,False")
                                     || formCollection["ProportionRows[" + i + "].Checked"].ToString().Equals("TRUE,FALSE"))
                             {
+                                // Mark the row as 'Checked'
                                 rowForSavingProportion.Checked = true;
                             }
                         }
 
+                        // Assign the index ID to the row
                         rowForSavingProportion.IndexID = formCollection["ProportionRows[" + i + "].IndexID"].ToString();
 
+                        // Assign the index Name to the row
                         rowForSavingProportion.IndexName = formCollection["ProportionRows[" + i + "].IndexName"].ToString();
 
+                        // Assign the proportion value to the row
                         if (formCollection["ProportionRows[" + i + "].Proportion"] != null)
                         {
                             try
@@ -93,26 +110,37 @@ namespace FBD.Controllers
                             }
                         }
 
+                        // Assign the proportion ID to the row
+                        // As default, the proportion ID is -1, and if the row exists in BusinessFinancialIndexProportion
+                        // table, the proportion ID will be assigned new integer value
                         rowForSavingProportion.ProportionID = int.Parse(
                                                         formCollection["ProportionRows[" + i + "].ProportionID"].ToString());
 
+                        // Add the row to the view model
                         viewModelForSavingProportion.ProportionRows.Add(rowForSavingProportion);
                     }
 
+                    // Get industry ID from View
                     viewModelForSavingProportion.IndustryID = formCollection["IndustryID"].ToString();
 
+                    // Saving the information with input is View Model created above
+                    // then return the error index
                     string errorIndex = BusinessFinancialIndexProportion
                                                     .EditFinancialIndexProportion(viewModelForSavingProportion);
 
+                    // If some errors occur
                     if (errorIndex != null)
                     {
+                        // Display error message when updating
                         TempData["Message"] = string.Format(CommonUtilities.Constants.ERR_UPDATE_FIPROPORTION, errorIndex);
                         FIProportionViewModel viewModelAfterError = BusinessFinancialIndexProportion
                                                             .CreateViewModelByIndustry(formCollection["IndustryID"].ToString());
                         return View(viewModelAfterError);
                     }
+                    // If no error occurs
                     else
                     {
+                        // Display successful message
                         TempData["Message"] = CommonUtilities.Constants.SCC_UPDATE_FIPROPORTION;
                         FIProportionViewModel viewModelAfterSuccess = BusinessFinancialIndexProportion
                                                             .CreateViewModelByIndustry(formCollection["IndustryID"].ToString());
@@ -122,6 +150,7 @@ namespace FBD.Controllers
             }
             catch (Exception)
             {
+                // Error message when handling in Controller
                 TempData["Message"] = CommonUtilities.Constants.ERR_POST_FIPROPORTION;
                 return RedirectToAction("Index");
             }
--- trunk/Sources/Source_Codes/FBDSource/FBD/ViewModels/FIProportionViewModel.cs
+++ trunk/Sources/Source_Codes/FBDSource/FBD/ViewModels/FIProportionViewModel.cs
@@ -6,12 +6,24 @@ using FBD.Models;
 
 namespace FBD.ViewModels
 {
+    /// <summary>
+    /// The view model is used to exchange data between View and Controller of FIProportion business process
+    /// </summary>
     public class FIProportionViewModel
     {
+        /// <summary>
+        /// The list of financial index row to be displayed on the View
+        /// </summary>
         public List<FIProportionRowViewModel> ProportionRows = new List<FIProportionRowViewModel>();
 
+        /// <summary>
+        /// The list of industries to be binded to the drop down list
+        /// </summary>
         public List<BusinessIndustries> Industries = new List<BusinessIndustries>();
 
+        /// <summary>
+        /// The industry id of selected industry name
+        /// </summary>
         public string IndustryID;
     }
 }
