55254e64b25138e70e6aea2feb666f3a4c64f69c
dragontl2189@gmail.com <dragontl2189@gmail.com@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-03-06 18:52:15 +0000

    Code model INV

---
+++ Sources/Source_Codes/FBDSource/FBD/Models/IndividualBasicIndexProportion.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FBD.ViewModels;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FBD.Models
+{
+    [MetadataType(typeof(IndividualBasicIndexProportionMetaData))]
+    public partial class IndividualBasicIndexProportion
+    {
+        /// <summary>
+        /// Select all the basic index proportion filtered by specified Borrowing purpose
+        /// </summary>
+        /// <param name="FBDModel">The Model of Entities Framework</param>
+        /// <param name="prmBorrowingPP">The selected Borrowing purpose</param>
+        /// <returns>List of basic index proportion</returns>
+        public static List<IndividualBasicIndexProportion> SelectBasicIndexProportionByBorrowingPP(
+                                                                    FBDEntities FBDModel, string prmBorrowingPPID)
+        {
+            List<IndividualBasicIndexProportion> lstINVProportionByBorrowingPP = FBDModel
+                                                                                .IndividualBasicIndexProportion
+                                                                                .Include("IndividualBasicIndex")
+                                                                                .Where(p => p.IndividualBorrowingPurposes
+                                                                                             .PurposeID
+                                                                                             .Equals(prmBorrowingPPID))
+                                                                                             .ToList();
+            return lstINVProportionByBorrowingPP;
+        }
+
+        /// <summary>
+        /// Select a single record in IndividualBasicIndexProportion with specified proportion id
+        /// </summary>
+        /// <param name="FBDModel">The Model of Entities Framework</param>
+        /// <param name="ProportionID">The proportion ID as primary key</param>
+        /// <returns>a record in IndividualBasicIndexProportion</returns>
+        public static IndividualBasicIndexProportion SelectBasicIndexProportionByProportionID(FBDEntities FBDModel,
+                                                                                                    int ProportionID)
+        {
+            //to: recheck about the exeption: null = null. however, we can't use .equal in null case
+            IndividualBasicIndexProportion BasicIndexProportion = FBDModel
+                                                                         .IndividualBasicIndexProportion
+                                                                         .First(p => p.ProportionID == ProportionID);
+            
+            return BasicIndexProportion;
+        }
+
+        /// <summary>
+        /// Add new a record to IndividualBasicIndexProportion
+        /// </summary>
+        /// <param name="FBDModel">The Model of Entities Framework</param>
+        /// <param name="viewModel">The View Model containing data</param>
+        /// <param name="row">The row to insert</param>
+        /// <returns></returns>
+        public static int AddBasicIndexProportion(FBDEntities FBDModel, INVProportionViewModel viewModel,
+                                                                        INVProportionRowViewModel row)
+        {
+            IndividualBasicIndexProportion BasicIndexProportion = new IndividualBasicIndexProportion();
+
+            // Select the business Borrowing purpose with specified Borrowing purpose ID
+            IndividualBorrowingPurposes borrowingPP = IndividualBorrowingPurposes
+                                                    .SelectBorrowingPPByID(viewModel.BorrowingPPID, FBDModel);
+            if (borrowingPP == null)
+            {
+                throw new Exception();
+            }
+
+            // Select the basic index with specified index ID
+            IndividualBasicIndex BasicIndex = IndividualBasicIndex
+                                                        .SelectBasicIndexByID(row.IndexID, FBDModel);
+            if (BasicIndex == null)
+            {
+                throw new Exception();
+            }
+
+            // Add new a row the IndividualBasicIndexProportion table
+            BasicIndexProportion.IndividualBorrowingPurposes = borrowingPP;
+            BasicIndexProportion.IndividualBasicIndex = BasicIndex;
+            BasicIndexProportion.Proportion = row.Proportion;
+
+            FBDModel.AddToIndividualBasicIndexProportion(BasicIndexProportion);
+            int temp = FBDModel.SaveChanges();
+
+            return temp <= 0 ? 0 : 1;
+        }
+
+        /// <summary>
+        /// Edit a single record of IndividualBasicIndexProportion
+        /// </summary>
+        /// <param name="FBDModel">The Model of Entities Framework</param>
+        /// <param name="row">The row to be edited</param>
+        /// <returns></returns>
+        public static int EditBasicIndexProportion(FBDEntities FBDModel, INVProportionRowViewModel row)
+        {
+            IndividualBasicIndexProportion BasicIndexProportion = SelectBasicIndexProportionByProportionID(
+                                                                                FBDModel, row.ProportionID);
+            BasicIndexProportion.Proportion = row.Proportion;
+            int temp = FBDModel.SaveChanges();
+
+            return temp <= 0 ? 0 : 1;
+        }
+
+        /// <summary>
+        /// Delete a single record of IndividualBasicIndexProportion
+        /// </summary>
+        /// <param name="FBDModel">The Model of Entities Framework</param>
+        /// <param name="ProportionID">The Proportion id as primary key</param>
+        /// <returns></returns>
+        public static int DeleteBasicIndexProportion(FBDEntities FBDModel, int ProportionID)
+        {
+            IndividualBasicIndexProportion deletedBasicIndexProportion = new IndividualBasicIndexProportion();
+            deletedBasicIndexProportion = FBDModel.IndividualBasicIndexProportion
+                                                .First(p => p.ProportionID == ProportionID);
+            FBDModel.DeleteObject(deletedBasicIndexProportion);
+            int temp = FBDModel.SaveChanges();
+
+            return temp <= 0 ? 0 : 1;
+        }
+
+        /// <summary>
+        /// Save information about proportion changes to the database
+        /// </summary>
+        /// <param name="viewModel">The View model containing data to be updated</param>
+        /// <returns>
+        /// The string indicates the basic index gets error, 
+        /// null value indicates successful updating
+        /// </returns>
+        public static string EditMultipleBasicIndexProportion(FBDEntities FBDModel, INVProportionViewModel viewModel)
+        {
+            // The error index is initially set to be empty, but not null;
+            string errorIndex = "";
+
+            try
+            {
+                // With each row in the list basic index of the view model got from View
+                foreach (var row in viewModel.ProportionRows)
+                {
+                    // Error index is temporarily assigned to the index id of the row
+                    // to display the error is got at which basic index if some
+                    // exceptions occur
+                    errorIndex = row.IndexID;
+
+                    // If the row is checked in the checkbox
+                    if (row.Checked == true)
+                    {
+                        // The proportion id less than 0 means that row does not
+                        // exist in the existing table of database.
+                        // With this situation, we add new a row to the table
+                        if (row.ProportionID < 0)
+                        {
+                            AddBasicIndexProportion(FBDModel, viewModel, row);
+                        }
+                        // If the row exists in the IndividualBasicIndexProportion table...
+                        else
+                        {
+                            // ...then update the row
+                            EditBasicIndexProportion(FBDModel, row);
+                        }
+                    }
+                    // If the row is not checked in checkbox...
+                    else
+                    {
+                        // ...and it exists in the table...
+                        if (row.ProportionID >= 0)
+                        {
+                            // ...then delete it
+                            DeleteBasicIndexProportion(FBDModel, row.ProportionID);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // If there is exception, return the string specifying the basic index gets error
+                return errorIndex;
+            }
+
+            // If update successfully, return null
+            return null;
+        }
+
+        /// <summary>
+        /// Create a view model used to exchange data between Controller and View of INVProportion business
+        /// </summary>
+        /// <param name="prmBorrowingPP">the selected Borrowing purpose chosen from drop down list of View</param>
+        /// <returns>The view model containing data to be displayed</returns>
+        public static INVProportionViewModel CreateViewModelByBorrowingPP(FBDEntities FBDModel, string prmBorrowingPP)
+        {
+            // The list of propotion got from IndividualBasicIndexProportion table with specified Borrowing purpose
+            List<IndividualBasicIndexProportion> lstINVProportionByBorrowingPP = new List<IndividualBasicIndexProportion>();
+
+            // The list of all basic indexes available in the system 
+            List<IndividualBasicIndex> lstBasicIndexes = new List<IndividualBasicIndex>();
+
+            // The view model that will be returned value
+            INVProportionViewModel viewModelResult = new INVProportionViewModel();
+
+            // Select all the basic index propotion with the specified Borrowing purpose
+            lstINVProportionByBorrowingPP = SelectBasicIndexProportionByBorrowingPP(FBDModel, prmBorrowingPP);
+
+            // Select all the basic indexes
+            lstBasicIndexes = FBDModel.IndividualBasicIndex.OrderBy(index => index.IndexID).ToList();
+
+            // With each basic indexes got from above process...
+            foreach (var index in lstBasicIndexes)
+            {
+                // ...Create each row for the returned view model
+                INVProportionRowViewModel viewModelRow = new INVProportionRowViewModel();
+
+                // Set value for each row...
+                viewModelRow.IndexID = index.IndexID;
+                viewModelRow.IndexName = index.IndexName;
+                viewModelRow.LeafIndex = index.LeafIndex;
+
+                // ...Then add the row to the returned view model
+                viewModelResult.ProportionRows.Add(viewModelRow);
+            }
+
+            // With each financial idnex that is enabled with the specified Borrowing purpose...
+            foreach (var item in lstINVProportionByBorrowingPP)
+            {
+                // ...And with each row in the returned view model...
+                foreach (var row in viewModelResult.ProportionRows)
+                {
+                    // ...Mark the row as 'Checked' if the basic index of the row
+                    // exists in the proportion of the specified Borrowing purpose
+                    if (item.IndividualBasicIndex.IndexID.Equals(row.IndexID))
+                    {
+                        row.Checked = true;
+                        row.Proportion = (decimal)item.Proportion;
+                        row.ProportionID = item.ProportionID;
+
+                        break;
+                    }
+                }
+            }
+
+            // Get all the industries in the system
+            viewModelResult.BorrowingPPs = FBDModel.IndividualBorrowingPurposes.ToList();
+            viewModelResult.BorrowingPPID = prmBorrowingPP;
+
+            return viewModelResult;
+        }
+        class IndividualBasicIndexProportionMetaData
+        {
+
+           
+            [StringLength(255)]
+            public decimal Proportion { get; set; }
+        }
+    }
+   
+}
---
+++ Sources/Source_Codes/FBDSource/FBD/Models/IndividualBorrowingPurposes.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FBD.Models
+{
+    [MetadataType(typeof(IndividualBorrowingPurposesMetaData))]
+    public partial class IndividualBorrowingPurposes
+    {
+        /// <summary>
+        /// Select all borrowing purpose in the table IndividualBorrowingPurposes
+        /// </summary>
+        /// <returns>List of borrowing purpose</returns>
+        public static List<IndividualBorrowingPurposes> SelectBorrowingPPList()
+        {
+            FBDEntities FBDModel = new FBDEntities();
+            List<IndividualBorrowingPurposes> lstFinancialIndex = null;
+            lstFinancialIndex = FBDModel.IndividualBorrowingPurposes.ToList();
+            return lstFinancialIndex;
+        }
+
+        /// <summary>
+        /// Select the borrowing purpose in the table IndividualBorrowingPurposes with purposeID= ID from input
+        /// </summary>
+        /// <param name="id">string ID</param>
+        /// <returns>IndividualBorrowingPurposes object</returns>
+        public static IndividualBorrowingPurposes SelectBorrowingPPByID(string id)
+        {
+            FBDEntities FBDModel = new FBDEntities();
+            IndividualBorrowingPurposes IndividualBorrowingPurposes = null;
+            IndividualBorrowingPurposes = FBDModel.IndividualBorrowingPurposes.First(pp => pp.PurposeID.Equals(id));
+            return IndividualBorrowingPurposes;
+        }
+
+        public static IndividualBorrowingPurposes SelectBorrowingPPByID(string id, FBDEntities FBDModel)
+        {
+            IndividualBorrowingPurposes IndividualBorrowingPurposes = null;
+            IndividualBorrowingPurposes = FBDModel.IndividualBorrowingPurposes.First(pp => pp.PurposeID.Equals(id));
+            return IndividualBorrowingPurposes;
+        }
+        /// <summary>
+        /// 1. Receive information from parameter
+        /// 2. Insert new index into the Database
+        /// 3. If successful, return 1 otherwise return 0
+        /// </summary>
+        /// <param name="businessFinancialIndex">A new FinancialIndex information</param>
+        /// <returns>Result code, 1 indicates success and 0 indicates error</returns> 
+        public static int AddBorrowingPP(IndividualBorrowingPurposes IndividualBorrowingPP)
+        {
+            FBDEntities FBDModel = new FBDEntities();
+            FBDModel.AddToIndividualBorrowingPurposes(IndividualBorrowingPP);
+            int temp = FBDModel.SaveChanges();
+            //it won't work for mutil-update
+            return temp <= 0 ? 0 : 1;
+
+        }
+
+        /// <summary>
+        /// Edit borrowing purpose
+        /// </summary>
+        /// <param name="IndividualBorrowingPP"></param>
+        /// <returns></returns>
+        public static int EditBorowingPurpose(IndividualBorrowingPurposes IndividualBorrowingPP)
+        {
+            FBDEntities entities = new FBDEntities();
+            var temp = entities.IndividualBorrowingPurposes.First(pp => pp.PurposeID == IndividualBorrowingPP.PurposeID);
+            temp.Purpose = IndividualBorrowingPP.Purpose;
+            
+            int result = entities.SaveChanges();
+            
+            return result <= 0 ? 0 : 1;
+        }
+
+        public static int DeleteBorrowingPurpose(string id)
+        {
+            FBDEntities entities = new FBDEntities();
+            var borrowingPP = entities.IndividualBorrowingPurposes.First(pp => pp.PurposeID == id);
+            entities.DeleteObject(borrowingPP);
+            int temp = entities.SaveChanges();
+
+            return temp <= 0 ? 0 : 1;
+        }
+
+        public class IndividualBorrowingPurposesMetaData
+        {
+            [DisplayName("Purpose ID")]
+            [Required(ErrorMessage = "Borrowing purpose ID is required")]
+            [StringLength(20)]
+            public string PurposeID { get; set; }
+
+            [DisplayName("Purpose ")]
+            [Required(ErrorMessage = "Purpose is required")]
+            [StringLength(255)]
+            public string Purpose { get; set; }
+        }
+    }
+}
