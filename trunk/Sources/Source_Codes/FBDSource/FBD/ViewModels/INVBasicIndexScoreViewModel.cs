bef5941b207cb50ac189e76a196ed6e1584927f5
dragontl2189@gmail.com <dragontl2189@gmail.com@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-03-06 18:52:44 +0000

    Code viewModel INV

---
+++ trunk/Sources/Source_Codes/FBDSource/FBD/ViewModels/INVBasicIndexScoreViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FBD.Models;
+
+namespace FBD.ViewModels
+{
+    public class INVBasicIndexScoreViewModel
+    {
+        /// <summary>
+        /// The list of business basicIndex to be binded to drop down list
+        /// </summary>
+        public List<IndividualBasicIndex> basicIndex = new List<IndividualBasicIndex>();
+
+        /// <summary>
+        /// The list of business BorrowingPP to be binded to drop down list
+        /// </summary>
+        public List<IndividualBorrowingPurposes> BorrowingPP = new List<IndividualBorrowingPurposes>();
+
+       
+        /// <summary>
+        /// The list or rows responsible for displaying information of financial index score
+        /// </summary>
+        public List<INVBasicScoreRowViewModel> ScoreRows = new List<INVBasicScoreRowViewModel>();
+
+        /// <summary>
+        /// The selected industry when selecting drop down list
+        /// </summary>
+        public string basicIndexID;
+
+        /// <summary>
+        /// The selected scale when selecting drop down list
+        /// </summary>
+        public string BorrowingPPID;
+
+        
+    }
+}
---
+++ trunk/Sources/Source_Codes/FBDSource/FBD/ViewModels/INVProportionRowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace FBD.ViewModels
+{
+    /// <summary>
+    /// The class represents for a row to be displayed on the list proportion of the INVProportion View
+    /// </summary>
+    [MetadataType(typeof(INVProportionRowViewModelMetaData))]
+    public class INVProportionRowViewModel
+    {
+        /// <summary>
+        /// The property to be displayed to the checkbox
+        /// True value indicates the checkbox is checked
+        /// As default, all the checkbox is unchecked
+        /// </summary>
+        public bool Checked = false;
+
+        /// <summary>
+        /// The field is got from table BusinessFinancialIndexProportion
+        /// to specify the primary key of the proportion
+        /// </summary>
+        public int ProportionID = -1;
+
+        /// <summary>
+        /// Index ID of the financial index
+        /// </summary>
+        public string IndexID;
+
+        /// <summary>
+        /// Index Name of the financial index
+        /// </summary>
+        public string IndexName;
+
+        /// <summary>
+        /// The leaf index indicates whether or not the financial index is used to calculate
+        /// </summary>
+        public bool LeafIndex;
+
+        /// <summary>
+        /// The proportion of the financial index with specified industry
+        /// </summary>
+        public decimal Proportion;
+
+        /// <summary>
+        /// Metadata class to validate the proportion
+        /// </summary>
+        public class INVProportionRowViewModelMetaData
+        {
+            public decimal Proportion;
+        }
+    }
+}
