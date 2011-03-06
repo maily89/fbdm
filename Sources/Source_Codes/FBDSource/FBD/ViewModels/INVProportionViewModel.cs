74265273903d4e575d414f30df73174e6235e8d4
dragontl2189@gmail.com <dragontl2189@gmail.com@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-03-06 18:52:44 +0000

    Code viewModel INV

---
+++ Sources/Source_Codes/FBDSource/FBD/ViewModels/INVProportionViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FBD.Models;
+
+namespace FBD.ViewModels
+{
+    /// <summary>
+    /// The view model is used to exchange data between View and Controller of INVProportion business process
+    /// </summary>
+    public class INVProportionViewModel
+    {
+        /// <summary>
+        /// The list of individual basic index row to be displayed on the View
+        /// </summary>
+        public List<INVProportionRowViewModel> ProportionRows = new List<INVProportionRowViewModel>();
+
+        /// <summary>
+        /// The list of borrowing purpose to be binded to the drop down list
+        /// </summary>
+        public List<IndividualBorrowingPurposes> BorrowingPPs = new List<IndividualBorrowingPurposes>();
+
+        /// <summary>
+        /// The BorrowingPP id of selected BorrowingPP name
+        /// </summary>
+        public string BorrowingPPID;
+    }
+}
