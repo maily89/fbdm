3aae6ea165526bfe99facb26d41838c2705de5de
mailydao@gmail.com <mailydao@gmail.com@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-03-08 05:11:49 +0000

    check proportion

--- trunk/Sources/Source_Codes/FBDSource/FBD/Scripts/CheckNull.js
+++ trunk/Sources/Source_Codes/FBDSource/FBD/Scripts/CheckNull.js
@@ -1,9 +1,13 @@
-function Check(){
+function Check() {
 	var total=0;
-	for (int i=0;i<$("#NumberOfProportionRows").val();i++){
-		if ($("#ProportionRows_"+i+"__Proportion") !=null){
-		total=total+$("#ProportionRows_"+i+"__Proportion").val();
+	for (i=0;i<$("#NumberOfProportionRows").val();i++){
+	    
+	    if ($("#ProportionRows_" + i.toString() + "__Proportion") != null) {
+	        
+		    total=parseFloat(total)+parseFloat($("#ProportionRows_"+i.toString()+"__Proportion").val());
 		}
-	}
-	return total;
+    }
+    if (total == 100) return true;
+    alert("Total percentage is not 100%");
+    return false;
 }
