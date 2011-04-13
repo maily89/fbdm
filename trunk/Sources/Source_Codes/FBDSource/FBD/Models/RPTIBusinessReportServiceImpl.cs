b2ef8f6d879c8270f628f8f8c2e12062895c6681
longnhi.kiemthanh <longnhi.kiemthanh@d234c362-8417-b006-dc4c-d754b9ca569c>
2011-04-13 06:56:25 +0000

    COMMENT CODE

--- trunk/Sources/Source_Codes/FBDSource/FBD/Models/RPTIBusinessReportServiceImpl.cs
+++ trunk/Sources/Source_Codes/FBDSource/FBD/Models/RPTIBusinessReportServiceImpl.cs
@@ -8,6 +8,17 @@ namespace FBD.Models
 {
     public class RPTIBusinessReportServiceImpl: RPTIBusinessReportService
     {
+        /// <summary>
+        /// 1. With input ID, select the following information:
+        /// CIFNumber, CustomerName, Industry, BusinessType, TaxCode, Branch, ReportingPeriod, Scale, FinancialScore,
+        /// NonFinancialScore, Rank, Evaluation
+        /// 2. Fill selected information to [businessInfo]
+        /// 3. Return 1 if success, otherwise return 0
+        /// </summary>
+        /// <param name="FBDModel">Model of EF</param>
+        /// <param name="ID">ID of business Ranking</param>
+        /// <param name="businessInfo">Business information to fill data</param>
+        /// <returns>integer indicates result</returns>
         public int FillGeneralInfo(FBDEntities FBDModel, int ID, RPTBusinessReportModel businessInfo)
         {
             try
@@ -37,6 +48,15 @@ namespace FBD.Models
             return 1;
         }
 
+        /// <summary>
+        /// 1. Select in table [CustomersBusinessScale] all scale information with inputted RankingID
+        /// 2. Fill information to [businessInfo].[ScaleInfo]
+        /// 3. Return 1 if success, otherwise return 0
+        /// </summary>
+        /// <param name="FBDModel">Model of EF</param>
+        /// <param name="RankingID">Ranking ID of business Ranking</param>
+        /// <param name="businessInfo">business information to fill data</param>
+        /// <returns>integer indicates result</returns>
         public int FillScaleInfo(FBDEntities FBDModel, int RankingID, RPTBusinessReportModel businessInfo)
         {
             try
@@ -61,6 +81,15 @@ namespace FBD.Models
             return 1;
         }
 
+        /// <summary>
+        /// 1. Select in table [CustomersBusinessFinancialIndex] all financial information with inputted RankingID
+        /// 2. Fill information to [businessInfo].[FinancialInfo]
+        /// 3. Return 1 if success, otherwise return 0
+        /// </summary>
+        /// <param name="FBDModel">Model of EF</param>
+        /// <param name="RankingID">Ranking ID of Business Ranking</param>
+        /// <param name="businessInfo">Business Information to fill data</param>
+        /// <returns>integer indicates result</returns>
         public int FillFinancialInfo(FBDEntities FBDModel, int RankingID, RPTBusinessReportModel businessInfo)
         {
             try
@@ -85,6 +114,15 @@ namespace FBD.Models
             return 1;
         }
 
+        /// <summary>
+        /// 1. Select in table [CustomersBusinessNonFinancialIndex] all non financial information with inputted RankingID
+        /// 2. Fill information to [businessInfo].[NonFinancialInfo]
+        /// 3. Return 1 if success, otherwise return 0
+        /// </summary>
+        /// <param name="FBDModel">Model of EF</param>
+        /// <param name="RankingID">Ranking ID of business ranking</param>
+        /// <param name="businessInfo">business information to fill data</param>
+        /// <returns>integer indicates result</returns>
         public int FillNonFinancialInfo(FBDEntities FBDModel, int RankingID, RPTBusinessReportModel businessInfo)
         {
             try
@@ -109,6 +147,14 @@ namespace FBD.Models
             return 1;
         }
 
+        /// <summary>
+        /// 1. Create RPTBusinessReportModel [businessInfo]
+        /// 2. Fill information to [businessInfo] with inputted ID
+        /// 3. Return [businessInfo]
+        /// </summary>
+        /// <param name="FBDModel">Model of EF</param>
+        /// <param name="ID">ID of business ranking</param>
+        /// <returns>Business Report model containing items to be displayed</returns>
         public RPTBusinessReportModel SelectBusinessInfo(FBDEntities FBDModel, int ID)
         {
             RPTBusinessReportModel businessInfo = new RPTBusinessReportModel();
--- trunk/Sources/Source_Codes/FBDSource/FBD/Models/RPTIIndividualReportService.cs
+++ trunk/Sources/Source_Codes/FBDSource/FBD/Models/RPTIIndividualReportService.cs
@@ -7,12 +7,55 @@ namespace FBD.Models
 {
     public interface RPTIIndividualReportService
     {
+        /// <summary>
+        /// 1. With input ID, select the following information:
+        /// _CIFNumber
+        /// _CustomerName
+        /// _BorrowingPurpose
+        /// _Branch
+        /// _RankedDate
+        /// _BasicScore
+        /// _CollateralScore
+        /// _Evaluation
+        /// 2. Fill selected information to [IndividualInfo]
+        /// 3. Return 1 if success, otherwise return 0
+        /// </summary>
+        /// <param name="FBDModel">Model of EF</param>
+        /// <param name="ID">ID of individual Ranking</param>
+        /// <param name="individualInfo">individual information to fill data</param>
+        /// <returns>integer indicates result</returns>
         int FillGeneralInfo(FBDEntities FBDModel, int ID, RPTIndividualReportModel individualInfo);
 
+        /// <summary>
+        /// 1. Select in table [CustomersIndividualBasicIndex] all basic information with inputted RankingID
+        /// 2. Fill information to [IndividualInfo].[BasicInfo]
+        /// 3. Return 1 if success, otherwise return 0
+        /// </summary>
+        /// <param name="FBDModel">Model of EF</param>
+        /// <param name="RankingID">Ranking ID of individual Ranking</param>
+        /// <param name="individualInfo">individual information to fill data</param>
+        /// <returns>integer indicates result</returns>
         int FillBasicInfo(FBDEntities FBDModel, int RankingID, RPTIndividualReportModel individualInfo);
 
+        /// <summary>
+        /// 1. Select in table [CustomersIndividualCollateralIndex] all collateral information with inputted RankingID
+        /// 2. Fill information to [IndividualInfo].[CollateralInfo]
+        /// 3. Return 1 if success, otherwise return 0
+        /// </summary>
+        /// <param name="FBDModel">Model of EF</param>
+        /// <param name="ID">ID of individual Ranking</param>
+        /// <param name="individualInfo">individual information to fill data</param>
+        /// <returns>integer indicates result</returns>
         int FillCollateralInfo(FBDEntities FBDModel, int RankingID, RPTIndividualReportModel individualInfo);
 
+        /// <summary>
+        /// 1. Create RPTIndividualReportModel [IndividualInfo]
+        /// 2. Fill information to [IndividualInfo] with inputted ID
+        /// 3. Return [IndividualInfo]
+        /// </summary>
+        /// <param name="FBDModel">Model of EF</param>
+        /// <param name="ID">ID of individual ranking</param>
+        /// <returns>individual Report model containing items to be displayed</returns>
         RPTIndividualReportModel SelectIndividualInfo(FBDEntities FBDModel, int ID);
     }
 }
